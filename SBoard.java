8bf4d460d92aabb960dfc2b77a7c19b7fcc453a7
vorpal56 <33470093+vorpal56@users.noreply.github.com>
2019-02-03 20:37:37 -0500

    work on GET request, Finished PIN/UNPIN, working on CLEAR

--- SBoard.java
+++ SBoard.java
@@ -5,32 +5,33 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Collections;
+import java.util.List;
 
 public class SBoard {
 	public static String defaultColour;
-	public static int boardWidth, boardHeight, portNumber;
+	public static int boardWidth, boardHeight, portNumber = 8000;
 	public static ArrayList<String> colours = new ArrayList<String>();
-	public static ArrayList<Note> notes = new ArrayList<Note>();
+	public static List<Note> notes = Collections.synchronizedList(new ArrayList<Note>());
 
-	//TODO dont through exception here - handle it
 	public static void main(String[] args) throws Exception {
-
-		//client.GUI gui = new client.GUI();
-		portNumber = Integer.parseInt(args[0]);
-		boardWidth = Integer.parseInt(args[1]);
-		//gui.verifyXCoord(boardWidth);
-		boardHeight = Integer.parseInt(args[2]);
-		//gui.verifyYCoord(boardHeight);
-		defaultColour = args[3];
-		for (int i = 3; i < args.length; i++) {
-			colours.add(args[i]);
+		try {
+			portNumber = Integer.parseInt(args[0]);
+			boardWidth = Integer.parseInt(args[1]);
+			boardHeight = Integer.parseInt(args[2]);
+			defaultColour = args[3];
+			for (int i = 3; i < args.length; i++) {
+				colours.add(args[i].toLowerCase());
+			}
+		} catch (Exception e) {
+			System.out.println("Bad values in command line");
+			System.exit(0);
 		}
-		//gui.setDefaultColor(defaultColour);
+
 		ServerSocket listener = new ServerSocket(portNumber);
 		try {
 			while (true) {
-				new Client(listener.accept()).start();
+				new Client(listener.accept(), boardWidth, boardHeight, colours).start();
 			}
 		} finally {
 			listener.close();
@@ -39,73 +40,34 @@ public class SBoard {
 
 	private static class Client extends Thread {
 		private Socket socket;
-		//private client.GUI gui;
 
-		public Client(Socket socket) {
+		public Client(Socket socket, int w, int h, List<String> colours) {
 			this.socket = socket;
-
-//			this.gui.resultArea.append("Client successful connection. \nWidth: " + Integer.toString(w) + " Height: "
-//					+ Integer.toString(h) + "\n");
-//			this.gui.resultArea.append("The colours are: \n");
-//			for (String colour : colours) {
-//				this.gui.resultArea.append(colour + "\n");
-//			}
 		}
 
 		@Override
 		public void run() {
 			try {
 
-				// Decorate the streams so we can send characters
-				// and not just bytes. Ensure output is flushed
-				// after every newline.
-
 				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
+				out.println("Connection successful. Width: " + boardWidth + " Height: " + boardHeight);
 				out.println(colours.toString());
+				// for (int i = 0; i < colours.size(); i++) {
+				// out.println(colours.get(i).toString() + "\n");
+				// }
 
-				// Send a welcome message to the client.
-				// out.println("Hello, you are client #" + clientNumber + ".");
-				// out.println("Enter a line with only a period to quit\n");
-
-				// Get messages from the client, line by line; return them
-				// capitalized
 				while (true) {
 					String input = in.readLine();
-					// from client "POST xCoord=" + x + ", yCoord=" + y + ", width=" + w + ", height=" + h + ", refersTo=" + message
-
-					if(input.startsWith("POST")){
-						int x=0, y=0, w=0, h=0;
-						String message="";
-						int index = input.indexOf("xCoord=") + "xCoord=".length();
-						while(input.charAt(index) != ','){
-							x = 10*x + input.charAt(index)-'0';
-							index++;
-						}
-						index = input.indexOf("yCoord=") + "yCoord=".length();
-						while(input.charAt(index) != ','){
-							y = 10*y + input.charAt(index)-'0';
-							index++;
-						}
-						index = input.indexOf("width=") + "width=".length();
-						while(input.charAt(index) != ','){
-							w = 10*w + input.charAt(index)-'0';
-							index++;
-						}
-						index = input.indexOf("height=") + "height=".length();
-						while(input.charAt(index) != ','){
-							h = 10*h + input.charAt(index)-'0';
-							index++;
-						}
-						
-						String[] messages = input.split("refersTo=");
-						message=messages[1];
-
-						log("New Note");
-						notes.add(new Note(x,y,w,h, null, false, message));
-						log(notes.get(notes.size()-1).toString());
+					if (input == null || input.equals("."))
+						break;
+					String message = null;
+					try {
+						message = requestType(input);
+					} catch (Exception e) {
+						// some
 					}
+					out.println(message);
 				}
 			} catch (IOException e) {
 				// log("Error handling client# " + clientNumber + ": " + e);
@@ -122,56 +84,163 @@ public class SBoard {
 		private void log(String message) {
 			System.out.println(message); // server side responses
 		}
+
+		private String requestType(String input) {
+			if (input != null) {
+				String[] request = input.split(" ");
+				String typeofreq = request[0];
+				if (typeofreq != null) {
+					String a = typeofreq.toUpperCase();
+					if (a.equals("GET")) {
+
+					} else if (a.equals("POST")) {
+						postNote(input);
+					} else if (a.equals("PIN")) {
+						pinLocation(input, 0);
+					} else if (a.equals("UNPIN")) {
+						pinLocation(input, 1);
+					} else if (a.equals("CLEAR")) {
+
+					} else if (a.equals("DISCONNECT")) {
+
+					}
+				} else {
+					// throw error
+				}
+			} else {
+				// throw error
+			}
+			return "hello";
+		}
+
+		private void postNote(String input) {
+			String message = "", colour = "";
+			int[] xyCoords = parseXY(input);
+			int[] wh = parseWH(input);
+			int x = xyCoords[0], y = xyCoords[1];
+			int w = wh[0], h = wh[1];
+			// parse the string and set it to each variable
+
+			int index = input.indexOf("color=") + "color=".length();
+			if (index == -1) {
+				colour = colours.get(0);
+			} else {
+				String[] colorArr = input.split("color=");
+				colour = colorArr[1];
+			}
+			Note newNote = new Note(x, y, w, h, colour, message);
+			String[] messages = input.split("refersTo=");
+			message = messages[1];
+
+			synchronized (notes) {
+				notes.add(newNote);
+			}
+			log(notes.get(notes.size() - 1).toString());
+
+		}
+
+		private void pinLocation(String input, int type) {
+			// parse the input
+			int[] xyCoords = parseXY(input);
+			int x = xyCoords[0], y = xyCoords[1];
+			synchronized (notes) {
+				for (Note note : notes) {
+					if (x >= note.getXCoord() && (x <= note.getWidth() + note.getXCoord()) && y <= note.getYCoord()
+							&& (y >= (note.getYCoord() - note.getHeight()))) {
+						if (type == 0) {
+							note.setPinStatus(true);
+						} else {
+							note.setPinStatus(false);
+						}
+					}
+				}
+			}
+
+		}
+
+		private void clear() {
+
+		}
+
+		private void disconnect() throws Exception {
+			try {
+				socket.close();
+			} finally {
+				System.out.println("Error closing connection");
+			}
+		}
+
+		private int[] parseXY(String input) {
+			int x = 0, y = 0;
+			int index = input.indexOf("xCoord=") + "xCoord=".length();
+			while (input.charAt(index) != ',') {
+				x = 10 * x + input.charAt(index) - '0';
+				index++;
+			}
+			index = input.indexOf("yCoord=") + "yCoord=".length();
+			while (input.charAt(index) != ',') {
+				y = 10 * y + input.charAt(index) - '0';
+				index++;
+			}
+			int[] xyCoords = { x, y };
+			return xyCoords;
+		}
+
+		private int[] parseWH(String input) {
+			int w = 0, h = 0;
+			int index = input.indexOf("width=") + "width=".length();
+			while (input.charAt(index) != ',') {
+				w = 10 * w + input.charAt(index) - '0';
+				index++;
+			}
+			index = input.indexOf("height=") + "height=".length();
+			while (input.charAt(index) != ',') {
+				h = 10 * h + input.charAt(index) - '0';
+				index++;
+			}
+			int[] wh = { w, h };
+			return wh;
+		}
+
 	}
 
-	public static class Note implements Comparable {
-		int xcoord, ycoord, width, height;
-		String colour, refersTo;
-		boolean isPinned;
+	public static class Note {
+		private int xcoord, ycoord, width, height;
+		private String colour, refersTo;
+		private boolean isPinned = false;
 
-		public Note(int xcoord, int ycoord, int width, int height, String colour, boolean isPinned, String refersTo) {
+		public Note(int xcoord, int ycoord, int width, int height, String colour, String refersTo) {
 			this.xcoord = xcoord;
 			this.ycoord = ycoord;
 			this.width = width;
 			this.height = height;
-			if (colour == null) {
-				this.colour = defaultColour;
-			}
+			this.colour = colour;
+			this.refersTo = refersTo;
 		}
 
-		@Override
-		// default compare notes by string
-		public int compareTo(Object arg0) {
-			// TODO Auto-generated method stub
-			return 0;
+		public void setPinStatus(boolean value) {
+			this.isPinned = value;
 		}
 
-		public String toString(){
-			return "" + this.ycoord + this.xcoord + this.width + this.height + this.colour;
-		}
-	}
-
-	public class locationComparator implements Comparator {
-
-		@Override
-		public int compare(Object o1, Object o2) {
-			Note note1 = (Note) o1;
-			Note note2 = (Note) o2;
-			// note1 comes before note2
-			return (note1.refersTo.toLowerCase().compareTo(note2.refersTo.toLowerCase()));
+		public int getXCoord() {
+			return this.xcoord;
 		}
 
-	}
-
-	public class colourComparator implements Comparator {
-
-		@Override
-		public int compare(Object o1, Object o2) {
-			Note note1 = (Note) o1;
-			Note note2 = (Note) o2;
-			return (note1.colour.toLowerCase().compareTo(note2.colour.toLowerCase()));
+		public int getYCoord() {
+			return this.ycoord;
 		}
 
+		public int getWidth() {
+			return this.width;
+		}
+
+		public int getHeight() {
+			return this.height;
+		}
+
+		public String getColour() {
+			return this.colour;
+		}
 	}
 
 }
